cc3ddc552eadd0a9b87621954f88a3dfa4cb9bd6
jsim0809 <jeremy_sim@hotmail.com>
2020-02-20 11:12:55 -0800

    Write videoListReducer.

--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,18 @@
 import Redux from 'redux';
 
 var videoListReducer = (state = sampleData, action) => {
-  //TODO: define a reducer for the videoList field of our state.
+  if (action.type === 'CHANGE_VIDEO_LIST') {
+    var newState = state.map((obj) => {
+      if (obj.videoList) {
+        return { videoList: action.videos };
+      } else {
+        return obj;
+      }
+    });
+    return newState;
+  } else {
+    return state;
+  }
 };
 
 export default videoListReducer;
